48a9708a62b8da766ca70b4f708e8cb4e53e6329
Robert Mill <r.mill@ntlworld.com>
2018-07-29 07:05:46 +0100

    first upload working files

---
+++ upload.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rmill_000
+ * Date: 27/07/2018
+ * Time: 16:57
+ */
+
+
+
+if( $_POST['fileToUpload']!= "" ) {
+
+    $target_dir = "uploads/";
+    $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+    $uploadOk = 1;
+    $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+// Check if image file is a actual image or fake image
+    if (isset($_POST["submit"])) {
+        $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+        if ($check !== false) {
+            echo "File is an image - " . $check["mime"] . ".";
+            $uploadOk = 1;
+        } else {
+            echo "File is not an image.";
+            $uploadOk = 0;
+        }
+
+    } else {
+        die("No file specified!");
+    }
+}
+
