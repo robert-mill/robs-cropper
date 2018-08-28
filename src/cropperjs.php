29f04f1a54dfd4d9e2b2c148782ddeedab7669ee
Robert Mill <r.mill@ntlworld.com>
2018-08-28 19:22:33 +0100

    Add files via upload

---
+++ src/cropperjs.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rmill_000
+ * Date: 28/08/2018
+ * Time: 19:16
+ */
+
+class cropperjs
+{
+    public static function world()
+    {
+        return 'Hello World, Composer!';
+    }
+}
